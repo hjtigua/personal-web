aab2598b4004a95013c51537aae7bfe7a2744887
hjtigua <tiguaguerrero@hotmail.com>
2022-04-04 18:51:40 -0500

    blog

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,5 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { faPersonDigging } from "@fortawesome/free-solid-svg-icons";
 
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div className="flex items-center justify-center flex-col text-2xl">
+      <span className="py-4">Hombres trabajando.</span>
+      <FontAwesomeIcon icon={faPersonDigging} size="10x" color="#F7BE3B" />
+    </div>
+  );
 }
