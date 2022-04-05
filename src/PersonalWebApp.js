98b79a948fbeb98af337cad612743f64623b1b9f
hjtigua <tiguaguerrero@hotmail.com>
2022-04-04 19:25:28 -0500

    contact

--- src/PersonalWebApp.js
+++ src/PersonalWebApp.js
@@ -11,7 +11,7 @@ export default function PersonalWebApp() {
     <>
       <div className="min-h-screen">
         <Header />
-        <div className="max-w-3xl mx-auto px-4 py-20">
+        <div className="max-w-4xl mx-auto px-4 py-20">
           <AppRouter />
         </div>
       </div>
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,32 @@
 import React from "react";
-
+import "./styles.css";
+import ilustracion from "../../assets/images/illustration.svg";
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="contact">
+      <figure className="contact__picture">
+        <img src={ilustracion} alt="loco" className="contact__img" />
+      </figure>
+      <form className="contact__form">
+        <h2 className="contact__title">Estamos en contacto</h2>
+        <input
+          type="text"
+          placeholder="Ingresa tu nombre"
+          className="contact__input"
+        />
+        <input
+          type="email"
+          placeholder="Ingresa tu email"
+          className="contact__input"
+        />
+        <textarea
+          placeholder="Mensaje..."
+          className="contact__input contact__input--area"
+        ></textarea>
+        <button type="submit" className="contact__cta">
+          Enviar ahora
+        </button>
+      </form>
+    </section>
+  );
 }
